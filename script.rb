1c03f84be98f34c8094a57690c3c7ad59519d626
weeezik <mkihanya.33@gmail.com>
2024-03-23 12:22:00 +0000

    Work on replacing spaces
    Still returns spaces as "%" at this version

--- script.rb
+++ script.rb
@@ -1,21 +1,20 @@
-#split characters into array
-# Example: ["H","e","l","l","o"]
-#add 3 positions to each character's alphabetical position, output new array
-# Example: ["K", "h", "o", "o", "r"]
-#Rejoin array use .join method
+#goes in as a parameter of caesar_cipher method: shift_factor = 3
 
-def caesar_cipher (string, shift_factor = 3)
-  codepoints_array = string.codepoints #identify each letter codepoint and return array
-  codepoints_array.map do |codepoint_letter|
-    new_codepoint = codepoint_letter + 3 #change its codepoint
-    new_letter = new_codepoint.chr
-    new_letter
+def caesar_cipher (string, shift_factor = 5)
+  codepoints_array = string.codepoints #split characters, convert to codepoint value, 
+  # return array of integer codepoint values
+  new_letter_array = codepoints_array.map do |codepoint|
+    new_codepoint = codepoint + shift_factor #change its codepoint by shift_factor positions
+    new_letter = new_codepoint.chr #convert from code point array to char array
+    new_letter #return array of new letter, like ["K", "h", "o", "o", "r"]
+    #replace "%" elements with " "
   end
-  #join function
-     #return its new value
-     #convert from code point array to char array
-     #convert from array to string
-   
-
-
+  newer_letter_array = new_letter_array.each {|letter| " " if letter == "%"}
+  newer_letter_array.join #convert from array to string (join function)
 end
+
+puts caesar_cipher("What a string")
+
+#Account for these:
+    #1. spaces! (% should be " ")
+    #2. wrap from a to z AND z to a
